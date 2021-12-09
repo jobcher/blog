ef1079b18fc4b51e4e1b685a1ceb69f35735e356
Administrator <admin@example.com>
2021-12-09 17:06:04 +0800

    上传

---
+++ content/posts/cloudcdn.md
@@ -0,0 +1,9 @@
+---
+title: "Cloudflare CDN 部署静态资源"
+date: 2021-12-09
+draft: true
+author: "sjtfreaks"
+tags: ["Cloudflare","部署前端"]
+---
+
+# Cloudflare CDN 部署静态资源
--- updatedocker.sh
+++ updatedocker.sh
@@ -5,7 +5,7 @@ docker stop nginx-hugo
 docker rm nginx-hugo
 docker rmi nginx:hugo
 echo "生成新的docker"
-hugo -t LoveIt
+hugo -t LoveIt -D
 docker build -t nginx:hugo .
 docker run --name nginx-hugo -d -p 8080:80 nginx:hugo
 echo "显示端口"
