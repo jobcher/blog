1caa9371207be48f634c43ea3099d28cb31bce7f
Administrator <admin@example.com>
2021-12-09 15:20:55 +0800

    提交

---
+++ updatedocker.sh
@@ -0,0 +1,11 @@
+#/!bin/bash
+echo "删除旧的docker"
+docker ps
+docker stop nginx-hugo
+docker rm nginx-hugo
+docker rmi nginx:hugo
+echo "生成新的docker"
+docker build -t nginx:hugo .
+docker run --name nginx-hugo -d  -p 8080:80 nginx:hugo
+echo "显示端口"
+netstat -lntp
