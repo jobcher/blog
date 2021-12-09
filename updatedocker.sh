505a026f4f496ff31f56b201c0fbd1c7b21c1159
sjtfreaks <13028911306@163.com>
2021-12-09 22:37:54 +0800

    docker 自启动

--- updatedocker.sh
+++ updatedocker.sh
@@ -7,6 +7,6 @@ docker rmi nginx:hugo
 echo "生成新的docker"
 hugo -t LoveIt -D
 docker build -t nginx:hugo .
-docker run --name nginx-hugo -d -p 8080:80 nginx:hugo
+docker run --name nginx-hugo -d -p 8080:80 --restart=always nginx:hugo 
 echo "显示端口"
 netstat -lntp
