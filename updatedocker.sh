2d942bd8b7d4fa3d50eeaa2015220fb97639be5f
Administrator <admin@example.com>
2021-12-09 16:27:03 +0800

    提交

--- updatedocker.sh
+++ updatedocker.sh
@@ -5,6 +5,7 @@ docker stop nginx-hugo
 docker rm nginx-hugo
 docker rmi nginx:hugo
 echo "生成新的docker"
+hugo -t LoveIt
 docker build -t nginx:hugo .
 docker run --name nginx-hugo -d -p 8080:80 nginx:hugo
 echo "显示端口"
