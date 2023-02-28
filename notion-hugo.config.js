76ce6bd5cb94b62daf8d30d1139c142a9570ff02
dev <dev@sanjiang.com>
2023-02-28 14:50:35 +0800

    更新

---
+++ notion-hugo.config.js
@@ -0,0 +1,23 @@
+module.exports = {
+    directory: "content_notion",
+    concurrency: 1,
+  
+    // saveAwsImageDirectory: null,
+    saveAwsImageDirectory: "tmp/downloads",
+  
+    // s3ImageUrlWarningEnabled: false,
+  
+    // s3ImageUrlReplaceEnabled: true,
+  
+    // s3ImageConvertToWebpEnalbed: true,
+  
+    downloadImageCallback: null,
+    customTransformerCallback: null,
+    authorName: "jobcher",
+    
+    // [Optional] setting, If you need.
+    // customProperties: [
+    //   ["ToC", "boolean"],
+    //   ["AdditionalDescription", "text"],
+    // ],
+  };
