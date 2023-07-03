3f7c736bb98f840643feb69a1ad279a09d66c341
dev <dev@sanjiang.com>
2023-07-03 10:48:44 +0800

    更新update

--- main.go
+++ main.go
@@ -12,7 +12,41 @@ import (
 )
 
 func main() {
-	// 发起 HTTP GET 请求
+	// 当前日期
+	today := time.Now().Format("2006-01-02")
+	md_name := "github_trending_" + today + ".md"
+
+	//判断文件是否存在
+	_, err := os.Stat("content/posts/github/" + md_name)
+	if err == nil {
+		fmt.Println("文件已存在")
+		os.Exit(0)
+	}
+
+	// 创建 Markdown 文件
+	file, err := os.Create("content/posts/github/" + md_name)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close()
+
+	// 写入 Markdown 文件头部
+	_, err = file.WriteString("---\ntitle: " + today + " GitHub 热门榜单\ndate: " + today + "\ndraft: true\nauthor: 'jobcher'\nfeaturedImage: '/images/github.png'\nfeaturedImagePreview: '/images/github.png'\ntags: ['github']\ncategories: ['github']\nseries: ['github']\n---\n\n")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// 获取github热门
+	get_github(md_name)
+	// 获取v2ex热门
+	get_v2ex(md_name)
+
+	fmt.Println("成功生成文件")
+}
+
+// 传入 md_name 参数
+func get_github(md_name string) {
+
 	res, err := http.Get("https://www.github.com/trending")
 	if err != nil {
 		log.Fatal(err)
@@ -25,30 +59,6 @@ func main() {
 
 	// 使用 goquery 解析 HTML
 	doc, err := goquery.NewDocumentFromReader(res.Body)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	// 当前日期
-	today := time.Now().Format("2006-01-02")
-	md_name := "github_trending_" + today + ".md"
-
-	//判断文件具体目录是否存在
-	_, err = os.Stat("content/posts/github/" + md_name)
-	if err == nil {
-		//文件存在
-		os.Remove(md_name)
-	}
-
-	// 创建 Markdown 文件
-	file, err := os.Create("content/posts/github/" + md_name)
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer file.Close()
-
-	// 写入 Markdown 文件头部
-	_, err = file.WriteString("---\ntitle: " + today + " GitHub 热门榜单\ndate: " + today + "\ndraft: true\nauthor: 'jobcher'\nfeaturedImage: '/images/github.png'\nfeaturedImagePreview: '/images/github.png'\ntags: ['github']\ncategories: ['github']\nseries: ['github']\n---\n\n")
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -78,11 +88,58 @@ func main() {
 		content += fmt.Sprintf("- URL: https://github.com%s\n", url)
 		content += fmt.Sprintf("- 作者: %s\n\n", author)
 
-		_, err = file.WriteString(content)
+		// 写入 Markdown 文件
+		file, err := os.OpenFile("content/posts/github/"+md_name, os.O_APPEND|os.O_WRONLY, 0644)
 		if err != nil {
 			log.Fatal(err)
 		}
-
+		defer file.Close()
+		file.WriteString(content)
 	})
-	fmt.Println("成功生成文件")
+
+}
+
+func get_v2ex(md_name string) {
+	// 发起 HTTP GET 请求
+	res, err := http.Get("https://www.v2ex.com/?tab=hot")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer res.Body.Close()
+
+	if res.StatusCode != 200 {
+		log.Fatalf("请求失败，状态码：%d", res.StatusCode)
+	}
+
+	// 使用 goquery 解析 HTML
+	doc, err := goquery.NewDocumentFromReader(res.Body)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// 查找所有的 trending repository
+	doc.Find(".cell.item").Each(func(i int, s *goquery.Selection) {
+		// 提取标题和作者,title 去除span标签
+		title := strings.TrimSpace(s.Find("span.item_title a").Text())
+		author := strings.TrimSpace(s.Find("strong a").First().Text())
+		url := strings.TrimSpace(s.Find("span.item_title a").AttrOr("href", ""))
+		desc := strings.TrimSpace(s.Find("span.topic_info").Text())
+
+		// 将信息以 Markdown 格式写入文件
+		content := fmt.Sprintf("### 排名 %d:", i+1)
+		content += fmt.Sprintf("%s\n", title)
+		content += fmt.Sprintf("- Description: %s\n", desc)
+		content += fmt.Sprintf("- URL: https://www.v2ex.com%s\n", url)
+		content += fmt.Sprintf("- 作者: %s\n\n", author)
+
+		fmt.Println(content)
+
+		// 写入 Markdown 文件
+		file, err := os.OpenFile("content/posts/github/"+md_name, os.O_APPEND|os.O_WRONLY, 0644)
+		if err != nil {
+			log.Fatal(err)
+		}
+		defer file.Close()
+		file.WriteString(content)
+	})
 }
