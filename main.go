1828467c199be5110d5972881134c70315d742aa
dev <dev@sanjiang.com>
2023-07-05 08:47:27 +0800

    新增微博热送

--- main.go
+++ main.go
@@ -31,17 +31,68 @@ func main() {
 	defer file.Close()
 
 	// 写入 Markdown 文件头部
-	_, err = file.WriteString("---\ntitle: " + today + " 打工人日报\ndate: " + today + "\ndraft: true\nauthor: 'jobcher'\nfeaturedImage: '/images/github.png'\nfeaturedImagePreview: '/images/github.png'\ntags: ['github']\ncategories: ['github']\nseries: ['github']\n---\n\n生活如潮水般涌来，我们都是打工人，拼搏在这个熙攘的都市。每天早出晚归，为了生计奔波不息。在这喧嚣的世界里，我们常常会忘记一些最基本的事情，比如保重身体，快乐摸鱼，以及提升自己。\n\n身体是革命的本钱。工作虽然重要，但如果我们身体垮了，一切都将付之东流。所以，无论多忙碌，我们都不能忽视保持身体的健康。多注意饮食，合理安排休息时间，保持适度的锻炼，这些都是我们保持身体健康的基本要求。身体健康，才能有充沛的精力投入工作，追求更美好的生活。\n\n快乐摸鱼是放松心情的好方法。打工人的工作压力常常让我们喘不过气来，但是，有时候放松一下也是必要的。不妨在闲暇时刻，去找一家喜欢的咖啡馆坐坐，或者约上几个好友一起逛逛街，欣赏风景。也可以在家里看一场喜剧电影，或者读一本有趣的小说。快乐的时光让我们重新充电，调整好状态，更好地迎接未来的挑战。\n\n")
+	_, err = file.WriteString("---\ntitle: " + today + " 打工人日报\ndate: " + today + "\ndraft: true\nauthor: 'jobcher'\nfeaturedImage: '/images/github.png'\nfeaturedImagePreview: '/images/github.png'\ntags: ['github']\ncategories: ['github']\nseries: ['github']\n---\n\n")
 	if err != nil {
 		log.Fatal(err)
 	}
 
+	// 获取微博热搜
+	get_weibo(md_name)
 	// 获取github热门
 	get_github(md_name)
 	// 获取v2ex热门
 	get_v2ex(md_name)
 
 	fmt.Println("成功生成文件")
+}
+
+func get_weibo(md_name string) {
+	//写入标题
+	file, err := os.OpenFile("content/posts/github/"+md_name, os.O_APPEND|os.O_WRONLY, 0644)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close()
+	file.WriteString("## 微博热搜榜\n\n")
+
+	// 发起 HTTP GET 请求
+	res, err := http.Get("https://s.weibo.com/top/summary?cate=realtimehot")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer res.Body.Close()
+
+	if res.StatusCode != 200 {
+		log.Fatalf("请求失败，状态码：%d", res.StatusCode)
+	}
+
+	// 使用 goquery 解析 HTML
+	doc, err := goquery.NewDocumentFromReader(res.Body)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// 查找所有的 trending repository
+	doc.Find(".list_a li").Each(func(i int, s *goquery.Selection) {
+		// 提取标题和作者,title 去除span标签
+		title := strings.TrimSpace(s.Find("a").Text())
+		url := strings.TrimSpace(s.Find("a").AttrOr("href", ""))
+
+		// 将信息以 Markdown 格式写入文件
+		content := fmt.Sprintf("#### %d.", i+1)
+		content += fmt.Sprintf("[%s]", title)
+		content += fmt.Sprintf("(https://s.weibo.com%s)\n", url)
+
+		fmt.Println(content)
+
+		// 写入 Markdown 文件
+		file, err := os.OpenFile("content/posts/github/"+md_name, os.O_APPEND|os.O_WRONLY, 0644)
+		if err != nil {
+			log.Fatal(err)
+		}
+		defer file.Close()
+		file.WriteString(content)
+	})
 }
 
 // 传入 md_name 参数
