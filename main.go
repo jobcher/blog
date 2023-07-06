7463c1a166e83f14fe05403d1809c91765da4cf7
dev <dev@sanjiang.com>
2023-07-06 16:42:39 +0800

    百度翻译

--- main.go
+++ main.go
@@ -1,11 +1,14 @@
 package main
 
 import (
+	"crypto/md5"
 	"encoding/json"
 	"fmt"
 	"io"
+	"io/ioutil"
 	"log"
 	"net/http"
+	"net/url"
 	"os"
 	"path/filepath"
 	"strings"
@@ -144,11 +147,19 @@ func get_github(md_name string) {
 
 		// 去除斜杠
 		author = strings.Replace(author, "/", "", -1)
+		//翻译
+		queryString := desc
+		result, err := translateString(queryString)
+		if err != nil {
+			fmt.Println("翻译失败：", err)
+			return
+		}
+		desc = result
 
 		// 将信息以 Markdown 格式写入文件
 		content := fmt.Sprintf("### 排名 %d:", i+1)
 		content += fmt.Sprintf("%s\n", title)
-		content += fmt.Sprintf("- Description: %s\n", desc)
+		content += fmt.Sprintf("- 简介: %s\n", desc)
 		content += fmt.Sprintf("- URL: https://github.com%s\n", url)
 		content += fmt.Sprintf("- 作者: %s\n\n", author)
 
@@ -287,4 +298,66 @@ func downloadBingWallpaper() {
 
 	fmt.Println("壁纸已成功保存到:", savePath)
 
+}
+
+// 翻译
+type TranslationResponse struct {
+	From         string `json:"from"`
+	To           string `json:"to"`
+	TransResults []struct {
+		Src string `json:"src"`
+		Dst string `json:"dst"`
+	} `json:"trans_result"`
+}
+
+func translateString(queryString string) (string, error) {
+	// 使用环境变量
+	apiKey := os.Getenv("BAIDU_TRANSLATE_API_KEY")
+	if apiKey == "" {
+		return "", fmt.Errorf("未设置百度翻译 API 密钥")
+	}
+	apiId := os.Getenv("BAIDU_TRANSLATE_API_ID")
+	if apiId == "" {
+		return "", fmt.Errorf("未设置百度翻译 API ID")
+	}
+	apiURL := "https://fanyi-api.baidu.com/api/trans/vip/translate"
+	salt := "1435660288" // 随机数，这里使用固定值
+
+	// 构建 POST 请求参数
+	values := url.Values{}
+	values.Set("q", queryString)
+	values.Set("from", "en")
+	values.Set("to", "zh")
+	values.Set("appid", apiId) // 百度翻译 API 的应用ID，固定值
+	sign := apiId + queryString + salt + apiKey
+	fmt.Println(sign)
+	values.Set("salt", salt)
+	values.Set("sign", fmt.Sprintf("%x", md5.Sum([]byte(sign))))
+
+	// 发送 POST 请求
+	resp, err := http.PostForm(apiURL, values)
+	if err != nil {
+		return "", fmt.Errorf("请求失败：%v", err)
+	}
+	defer resp.Body.Close()
+
+	// 读取响应内容
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return "", fmt.Errorf("读取响应失败：%v", err)
+	}
+
+	// 解析 JSON 数据
+	var response TranslationResponse
+	err = json.Unmarshal(body, &response)
+	if err != nil {
+		return "", fmt.Errorf("解析 JSON 失败：%v", err)
+	}
+
+	// 提取翻译结果
+	if len(response.TransResults) > 0 {
+		return response.TransResults[0].Dst, nil
+	}
+
+	return "", fmt.Errorf("未找到翻译结果")
 }
